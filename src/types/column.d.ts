bd76e35be7979511d92a4772d3cbc6f9660e8671
Sven van de Scheur <svenvandescheur@gmail.com>
2023-03-09 16:54:11 +0100

    Update src/types/column.d.ts
    Co-authored-by: Sergei Maertens <sergei-maertens@users.noreply.github.com>

--- src/types/column.d.ts
+++ src/types/column.d.ts
@@ -1,17 +1,8 @@
 import { IFormioComponent } from './component'
 
+type ColumnSize = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 export interface IFormioColumn {
-  components: IFormioComponent[]
-
-  currentWidth: number
-
-  offset: number
-
-  pull: number
-
-  push: number
-
-  sizeMobile: number
-
-  width: number
+  components: IFormioComponent[];
+  size: ColumnSize;
+  sizeMobile?: ColumnSize;
 }
