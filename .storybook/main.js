f5be3eabb2600f128b2804ee68382d19686230ba
Sergei Maertens <sergei@maykinmedia.nl>
2023-04-03 12:36:44 +0200

    :heavy_plus_sign: [#21] -- set up storybook webpack5 builder

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,26 +1,27 @@
-const path = require('path')
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
   ],
   core: {
-    disableTelemetry: true
+    disableTelemetry: true,
+    builder: 'webpack5',
   },
   framework: '@storybook/react',
 
-  webpackFinal: (config) => {
-    config.resolve.plugins = config.resolve.plugins || []
+  webpackFinal: config => {
+    config.resolve.plugins = config.resolve.plugins || [];
     config.resolve.plugins.push(
       new TsconfigPathsPlugin({
-        configFile: path.resolve(__dirname, '../tsconfig.json')
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
       })
-    )
+    );
 
-    return config
-  }
-}
+    return config;
+  },
+};
