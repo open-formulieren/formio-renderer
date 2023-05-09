6caa4f6cabb3142859b8bc19e98e0ca231c36a23
Sven van de Scheur <sven@maykinmedia.nl>
2023-05-09 17:04:09 +0200

    :memo: #30 - Add story for custom component.

---
+++ src/tests/custom-component/custom-component.stories.tsx
@@ -0,0 +1,96 @@
+import {DEFAULT_RENDER_CONFIGURATION, RenderForm} from '@lib/renderer';
+import {DEFAULT_VALIDATORS} from '@lib/validation';
+import {expect} from '@storybook/jest';
+import type {ComponentStory, Meta} from '@storybook/react';
+import {userEvent, within} from '@storybook/testing-library';
+import {IComponentProps} from '@types';
+import React from 'react';
+
+const meta: Meta<typeof RenderForm> = {
+  title: 'Usage / Configuration / Custom Components',
+  component: RenderForm,
+  decorators: [],
+  parameters: {},
+};
+export default meta;
+
+DEFAULT_VALIDATORS[3][1] = 'Vul het verplichte veld in';
+
+const BootstrapInput: React.FC<IComponentProps> = ({callbacks, component, errors, value}) => (
+  <>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+      crossOrigin="anonymous"
+    />
+    <label className="form-label" htmlFor="firstName">
+      {component.label}
+    </label>
+    <div className={`input-group${errors.length ? ' has-validation' : ''}`}>
+      <input
+        id="firstName"
+        type="text"
+        className={`form-control${errors.length ? ' is-invalid' : ''}`}
+        name={component.key}
+        value={String(value || '')}
+        {...callbacks}
+      />
+
+      {errors.map(error => (
+        <div key={error} className="invalid-feedback">
+          {error}
+        </div>
+      ))}
+    </div>
+  </>
+);
+
+export const renderFormWithCustomComponent: ComponentStory<typeof RenderForm> = args => (
+  <RenderForm {...args} />
+);
+renderFormWithCustomComponent.args = {
+  configuration: {
+    ...DEFAULT_RENDER_CONFIGURATION,
+    components: {
+      textfield: BootstrapInput,
+    },
+  },
+  form: {
+    display: 'form',
+    components: [
+      {
+        type: 'textfield',
+        key: 'firstName',
+        label: 'First name',
+        validate: {
+          required: true,
+        },
+      },
+    ],
+  },
+};
+renderFormWithCustomComponent.play = async ({canvasElement}) => {
+  // Context.
+  const canvas = within(canvasElement);
+  const input = canvas.getByLabelText('First name');
+
+  // Assert rendered component is configured (Bootstrap) component.
+  expect(input.classList).toContain('form-control');
+
+  // Assert rendered component shows no error when invalid and pristine.
+  await userEvent.clear(input);
+  expect(await canvas.queryByText('Vul het verplichte veld in')).toBeNull();
+
+  // Assert rendered component shows no error when valid.
+  await userEvent.type(input, 'John', {delay: 30});
+  expect(await canvas.queryByText('Vul het verplichte veld in')).toBeNull();
+
+  // Assert rendered component shows error when invalid.
+  await userEvent.clear(input);
+  await canvas.findByText('Vul het verplichte veld in');
+
+  // Assert rendered component removes error when valid.
+  await userEvent.type(input, 'John', {delay: 30});
+  expect(await canvas.queryByText('Vul het verplichte veld in')).toBeNull();
+};
