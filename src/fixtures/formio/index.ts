4f06c849ea117ae46a0ec6abdd24d0dff0400c19
Nikki Ysendoorn <nikki.ysendoorn@maykinmedia.nl>
2024-03-12 10:24:16 +0100

    :fire: [#48] remove src to start over

--- src/fixtures/formio/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './formio-columns';
-export * from './formio-example';
-export * from './formio-length';
-export * from './formio-pattern';
-export * from './formio-required';
--- src/lib/format/placeholder.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Substitutes {{ placeholder }} with context[placeholder} in template string.
- * TODO: This might need refactoring to suit translations.
- */
-export const substitute = (template: string, context: {[index: string]: any}) => {
-  return template.replace(/{{\s*(\w+)\s*}}/g, (_: string, key: string) => context[key] || '');
-};
