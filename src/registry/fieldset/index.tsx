ea285fa1af48908959039683e99d7f0c4c5525ad
Sergei Maertens <sergei@maykinmedia.nl>
2024-12-22 17:47:09 +0100

    :sparkles: Have the fieldset component recurse its children
    Fieldsets primary purpose is rendering the children contained inside.

--- src/registry/fieldset/index.tsx
+++ src/registry/fieldset/index.tsx
@@ -1,11 +1,20 @@
 import type {FieldsetComponentSchema} from '@open-formulieren/types';
 
+import FormioComponent from '@/components/FormioComponent';
+
 export interface FieldsetProps {
   componentDefinition: FieldsetComponentSchema;
 }
 
 const Fieldset: React.FC<FieldsetProps> = ({componentDefinition: {components}}) => {
-  return <div>I am a fieldset field with {components.length} child components.</div>;
+  return (
+    <div>
+      I am a fieldset field with {components.length} child components. The nested children are:
+      {components.map(nestedDefinition => (
+        <FormioComponent key={nestedDefinition.id} componentDefinition={nestedDefinition} />
+      ))}
+    </div>
+  );
 };
 
 export default Fieldset;
