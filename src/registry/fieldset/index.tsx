88be1ed5a91c061ee18c0aa42e23c5afec40cc9b
Sergei Maertens <sergei@maykinmedia.nl>
2024-12-24 21:10:12 +0100

    :lipstick: Implement proper layout and styling for fieldset
    Taken from Open Forms SDK and slightly adapted.

--- src/registry/fieldset/index.tsx
+++ src/registry/fieldset/index.tsx
@@ -1,6 +1,10 @@
 import type {FieldsetComponentSchema} from '@open-formulieren/types';
+import clsx from 'clsx';
 
+import FormFieldContainer from '@/components/FormFieldContainer';
 import type {FormioComponentProps} from '@/components/FormioComponent';
+
+import './Fieldset.scss';
 
 export interface FieldsetProps {
   componentDefinition: FieldsetComponentSchema;
@@ -8,16 +12,19 @@ export interface FieldsetProps {
 }
 
 const Fieldset: React.FC<FieldsetProps> = ({
-  componentDefinition: {components},
+  // TODO: display tooltip, if specified
+  componentDefinition: {components, hideHeader = false, label},
   renderNested: FormioComponent,
 }) => {
   return (
-    <div>
-      I am a fieldset field with {components.length} child components. The nested children are:
-      {components.map(nestedDefinition => (
-        <FormioComponent key={nestedDefinition.id} componentDefinition={nestedDefinition} />
-      ))}
-    </div>
+    <fieldset className={clsx('openforms-fieldset', {'openforms-fieldset--no-header': hideHeader})}>
+      {!hideHeader && label && <legend className="openforms-fieldset__legend">{label}</legend>}
+      <FormFieldContainer>
+        {components.map(nestedDefinition => (
+          <FormioComponent key={nestedDefinition.id} componentDefinition={nestedDefinition} />
+        ))}
+      </FormFieldContainer>
+    </fieldset>
   );
 };
 
