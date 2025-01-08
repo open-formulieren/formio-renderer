216d7b8ea2f3807f182377f0ebbfef767e536266
Sergei Maertens <sergei@maykinmedia.nl>
2025-01-08 22:21:46 +0100

    :sparkles: First pass at implementing the formio email component type
    TODO: validation of the actual email value
    TODO: see if we need to do anything with validateOn

---
+++ src/registry/email/index.tsx
@@ -0,0 +1,26 @@
+import type {EmailComponentSchema} from '@open-formulieren/types';
+
+import TextField from '@/components/forms/TextField';
+
+export interface FormioEmailProps {
+  componentDefinition: EmailComponentSchema;
+}
+
+const FormioEmail: React.FC<FormioEmailProps> = ({
+  componentDefinition: {key, label, description, placeholder, validate, autocomplete},
+}) => {
+  return (
+    <TextField
+      name={key}
+      type="email"
+      label={label}
+      description={description}
+      isRequired={validate?.required}
+      placeholder={placeholder}
+      autoComplete={autocomplete}
+    />
+  );
+};
+
+export default FormioEmail;
+export {default as getInitialValues} from './initialValues';
