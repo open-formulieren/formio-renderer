22218ccf541c12bbb85c562ce32ab81bb2f8c9e8
Sergei Maertens <sergei@maykinmedia.nl>
2025-01-26 22:03:20 +0100

    :recycle: Export TextField composite from components/forms
    This requires restructuring the component imports to avoid circular
    imports.

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -1,3 +1,7 @@
+// Input field types
+export {default as TextField} from './TextField';
+
+// Helpers
 export {default as Label} from './Label';
 export {default as HelpText} from './HelpText';
 export {default as ValidationErrors} from './ValidationErrors';
