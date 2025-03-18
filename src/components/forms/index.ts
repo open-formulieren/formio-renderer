bf21e3f244757834c0e75c806242284140c52267
Sergei Maertens <sergei@maykinmedia.nl>
2025-03-18 20:26:17 +0100

    :package: Fix missing EditGrid export

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -3,6 +3,9 @@ export {default as TextField} from './TextField';
 export {default as RadioField} from './RadioField';
 export {default as DateField} from './DateField';
 
+// Complex form inputs
+export {default as EditGrid} from './EditGrid';
+
 // Helpers
 export {default as Label} from './Label';
 export {default as HelpText} from './HelpText';
