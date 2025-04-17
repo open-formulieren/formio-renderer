8a1b965e11c7689e73242f080cb73d560575feab
Sergei Maertens <sergei@maykinmedia.nl>
2025-04-17 12:22:17 +0200

    :sparkles: [#98] Support tooltips in the formio fieldset component
    This one still uses our own custom styling, for which users likely
    have configured design tokens. Eventually, we will convert it to the
    NL DS community component (utrecht-fieldset), but for the tooltip
    stories that's out of scope.
    
    However, we do support those existing tooltip design tokens in our
    custom fieldset styles to make it easier to apply consistent styling.

--- src/registry/fieldset/index.tsx
+++ src/registry/fieldset/index.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 
 import FormFieldContainer from '@/components/FormFieldContainer';
 import type {FormioComponentProps} from '@/components/FormioComponent';
+import Tooltip from '@/components/forms/Tooltip';
 import type {RegistryEntry} from '@/registry/types';
 
 import './Fieldset.scss';
@@ -16,13 +17,21 @@ export interface FieldsetProps {
 }
 
 export const Fieldset: React.FC<FieldsetProps> = ({
-  // TODO: display tooltip, if specified
-  componentDefinition: {components, hideHeader = false, label},
+  componentDefinition: {components, hideHeader = false, label, tooltip},
   renderNested: FormioComponent,
 }) => {
   return (
     <fieldset className={clsx('openforms-fieldset', {'openforms-fieldset--no-header': hideHeader})}>
-      {!hideHeader && label && <legend className="openforms-fieldset__legend">{label}</legend>}
+      {!hideHeader && label && (
+        <legend
+          className={clsx('openforms-fieldset__legend', {
+            'openforms-fieldset__legend--tooltip': !!tooltip,
+          })}
+        >
+          {label}
+          {tooltip && <Tooltip>{tooltip}</Tooltip>}
+        </legend>
+      )}
       <FormFieldContainer>
         {components.map(nestedDefinition => (
           <FormioComponent key={nestedDefinition.id} componentDefinition={nestedDefinition} />
