5f3e9351db382bba824621679e54ffb757a19534
Sergei Maertens <sergei@maykinmedia.nl>
2025-05-16 16:25:27 +0200

    :white_check_mark: [#59] Add test for expected conditional behaviour in editgrid

--- src/reference/hidden.stories.ts
+++ src/reference/hidden.stories.ts
@@ -186,3 +186,102 @@ const {custom: ConditionallyNestedVisible, reference: ConditionallyNestedVisible
   });
 
 export {ConditionallyNestedVisible, ConditionallyNestedVisibleReference};
+
+const {custom: ConditionallyVisibleInEditGrid, reference: ConditionallyVisibleInEditGridReference} =
+  storyFactory({
+    args: {
+      components: [
+        {
+          type: 'textfield',
+          id: 'externalTrigger',
+          key: 'externalTrigger',
+          label: 'Trigger outside editgrid',
+        },
+        {
+          type: 'editgrid',
+          id: 'editgrid',
+          key: 'editgrid',
+          label: 'Edit grid',
+          disableAddingRemovingRows: false,
+          groupLabel: 'Item',
+          hideLabel: false,
+          // @ts-expect-error
+          inlineEdit: false,
+          components: [
+            {
+              type: 'textfield',
+              id: 'nestedTrigger',
+              key: 'nestedTrigger',
+              label: 'Trigger inside editgrid',
+            },
+            {
+              type: 'textfield',
+              id: 'follower1',
+              key: 'follower1',
+              label: 'Follower 1',
+              conditional: {
+                when: 'externalTrigger',
+                eq: 'show follower 1',
+                show: true,
+              },
+            },
+            {
+              type: 'textfield',
+              id: 'follower2',
+              key: 'follower2',
+              label: 'Follower 2',
+              conditional: {
+                when: 'editgrid.nestedTrigger',
+                eq: 'show follower 2',
+                show: true,
+              },
+            },
+          ],
+          defaultValue: [],
+        },
+      ],
+    },
+    play: async ({canvasElement, step}) => {
+      const canvas = within(canvasElement);
+
+      const externalTriggerField = await canvas.findByLabelText('Trigger outside editgrid');
+      expect(externalTriggerField).toBeVisible();
+
+      // Add two items to the edit grid, but don't edit them yet. Note: the lookup needs
+      // to be done multiple times because Formio re-renders the element in the DOM...
+      await userEvent.click(await canvas.findByRole('button', {name: /Add/}));
+      await userEvent.click(await canvas.findByRole('button', {name: /Add/}));
+      expect(canvas.queryByLabelText('Follower 1')).not.toBeInTheDocument();
+      expect(canvas.queryByLabelText('Follower 2')).not.toBeInTheDocument();
+
+      await step('Visibility trigger because of external trigger', async () => {
+        await userEvent.type(externalTriggerField, 'show follower 1');
+
+        // Formio renders the same ID for multiple elements in repeating groups. :)
+        // So we can't do getAllByLabelText
+        await waitFor(() => {
+          const follower1Fields = canvas.getAllByText('Follower 1');
+          expect(follower1Fields).toHaveLength(2);
+        });
+      });
+
+      // here the logic/conditional is applied to the scope of a single item in the
+      // repeating group, while the external trigger affects *all* items
+      await step('Visibility trigger with nested trigger', async () => {
+        const firstNestedTrigger = canvas.getAllByLabelText('Trigger inside editgrid')[0];
+        await userEvent.type(firstNestedTrigger, 'show follower 2');
+
+        // Formio renders the same ID for multiple elements in repeating groups. :)
+        // So we can't do getAllByLabelText
+        await waitFor(() => {
+          const follower1Fields = canvas.getAllByText('Follower 1');
+          expect(follower1Fields).toHaveLength(2);
+
+          const follower2Fields = canvas.getAllByText('Follower 2');
+          expect(follower2Fields).toHaveLength(1);
+        });
+      });
+    },
+  });
+
+export {ConditionallyVisibleInEditGrid, ConditionallyVisibleInEditGridReference};
