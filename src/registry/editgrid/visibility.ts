1d3cd2f89263109a51e515ce3bc2e50237a58bae
Sergei Maertens <sergei@maykinmedia.nl>
2025-07-18 15:31:38 +0200

    :sparkles: [#59] Ensure that edit grids support clearOnHide behaviour
    The architecture rework was needed because of edit grids because the
    simple model of layout components doesn't apply here:
    
    * the nested components definition is not static for each item
    * there is no explicitly defined components configuration for each
      item, instead they're 'virtual' and the blueprint is provided by
      the edit grid component itself
    * when dealing with nested edit grids and their preview/edit mode, you
      can't rely on the React/Formik state to persist the updated values
      back into the parent value/state
    
    So, this rework splits the value/state management and the presentation
    logic. `applyVisibility` is implemented and called recursively from
    the top-level component. It's responsible for making sure the Formik
    values state is synchronized with the visibility state. It can't assign
    a modified list of components for each item anywhere. It is active at
    all times, whether an item is in preview mode or edit mode.
    
    The `ItemBody` and `ItemPreview` components also invoke the
    visibility processing. The former synchronizes the updated item values
    back to the Formik state, while the latter is purely presentational. It
    must be present, because for nested edit grids, ItemBody is not called
    for preview mode.
    
    This setup does simplify the item preview, because we now have the
    guarantee that the proper evaluation scope is passed to `ItemPreview`.

---
+++ src/registry/editgrid/visibility.ts
@@ -0,0 +1,54 @@
+import type {EditGridComponentSchema} from '@open-formulieren/types';
+import {getIn, replace, setIn} from 'formik';
+
+import type {ApplyVisibility} from '@/registry/types';
+import type {JSONObject} from '@/types';
+import {extractInitialValues} from '@/values';
+import {processVisibility} from '@/visibility';
+
+const applyVisibility: ApplyVisibility<EditGridComponentSchema> = (
+  componentDefinition,
+  values,
+  context
+) => {
+  const {key, components} = componentDefinition;
+  const initialValues = extractInitialValues(components, context.getRegistryEntry);
+
+  // ensure we keep adding to the parent scope in case of nested edit grids
+  const outerEvaluationScope = context?.getEvaluationScope?.(values) ?? values;
+
+  let items: JSONObject[] = getIn(values, key) ?? [];
+  for (let index: number = 0; index < items.length; index++) {
+    const itemValues = items[index];
+
+    const getEvaluationScope = (itemValues: JSONObject): JSONObject => {
+      const evaluationScope: JSONObject = setIn(outerEvaluationScope, key, itemValues);
+      return evaluationScope;
+    };
+
+    // we cannot process visibleComponents and omit the hidden ones, as there is no
+    // definition for a single item -> the display layer needs to do this. We can only
+    // process the value mutations, which (in turn) drive the presentation.
+    const {updatedValues: updatedItemValues} = processVisibility(components, itemValues, {
+      ...context,
+      initialValues,
+      getEvaluationScope,
+    });
+
+    // process an update by replace the item
+    if (updatedItemValues !== itemValues) {
+      items = replace(items, index, updatedItemValues) as JSONObject[];
+    }
+  }
+
+  // update the array value of the edit grid - if no items were changed, this is an
+  // identity update and no state changes will trigger
+  const updatedValues: JSONObject = setIn(values, key, items);
+
+  return {
+    updatedDefinition: componentDefinition,
+    updatedValues: updatedValues,
+  };
+};
+
+export default applyVisibility;
