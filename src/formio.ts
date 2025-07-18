68736a3bb4907396873052b5cabf2dbd23da3c15
Sergei Maertens <sergei@maykinmedia.nl>
2025-07-18 15:22:59 +0200

    :building_construction: [#59] Rework how we handle component visibility
    The previous iteration *did* take into account form/submission values
    to evaluate conditional visibility, but didn't account for the side
    effects this produces through the `clearOnHide` feature.
    
    The short summary is: values determine whether a component is visible
    or not (so they affect 'component configuration'), BUT this is not a
    one-way street - the visibility state/configuration of a component
    affects values through the `clearOnHide` behaviour.
    
    The code dealing with this has been renamed and restructured. The main
    idea is:
    
    * as input, take an array of components of which you want to see the
      visibility state, and an object with values for the conditional
      evaluatio scope
    * as output, you get an array of visible components and updated values
    
    The component array recurses so that layout components can apply the
    visibility state to their nested children immediately. Values are also
    updated immediately, and the updated result is fed to the next
    component and/or item, which helps minimize evaluation cycles.
    
    The result is put back in the React component/Formik state, i.e. the
    values are applied back to the formik state *if* there are updates. We
    are very careful in only mutating when needed, using the setIn and
    replace helpers from Formik. If the input values are equal to the
    output values, we guarantee that the object identities are the same
    and React will not render unnecessarily (otherwise you'd end up in an
    infinite render loop).
    
    This architecture also deliberately separates the 'submission values'
    and 'evaluation scope' concepts. Most of the time they're the same,
    but Formio's edit grids work differently, where each item is
    essentially assigned to a different scope when evaluating it. This
    evaluation scope can not be assigned back to the item values, since it
    contains values from its outer scope too. The extraEvaluationScope that
    existed before has been replaced with a callback so that the parent
    caller can provide the scope through a closure, as only the parent node
    has the necessary context.

--- src/formio.ts
+++ src/formio.ts
@@ -25,9 +25,14 @@ export const getConditional = (component: AnyComponentSchema): Conditional | nul
 };
 
 /**
- * Determine if a component is visible or hidden, depending on the *current* values.
+ * Given an evaluation scope of values, determine if a component is visible or hidden.
+ *
+ * Note that the `evaluationScope` is often equal to the form values of all components,
+ * but not always - e.g. when using edit grids, the item values of each item are
+ * scoped inside their parent key so that components can have conditional logic based
+ * on their siblings components.
  */
-export const isHidden = (component: AnyComponentSchema, values: JSONObject): boolean => {
+export const isHidden = (component: AnyComponentSchema, evaluationScope: JSONObject): boolean => {
   // dynamic hidden/visible configuration
   const conditional = getConditional(component);
 
@@ -43,12 +48,32 @@ export const isHidden = (component: AnyComponentSchema, values: JSONObject): boo
   // result.
   const {show, when, eq} = conditional;
 
-  // TODO: how does the scoping of the 'when' expression work for nesting and repeating
-  // groups? -> check backend code and formio reference.
-  const compareValue = getIn(values, when);
+  // TODO: ensure comparison/check works for Array values (e.g. textfield with multiple: true)
+  // TODO: ensure comparison/check works for selectboxes with their weird data format!
+
+  // NOTE: Formio defaults to an empty string if the value is null-ish (null | undefined),
+  // which makes things work when the value has been cleared by an earlier pass. It's a bit
+  // shaky - in particular for `number` components I'd prefer sticking to `null` and making
+  // strict comparisons like that, but let's explore that when we've actually shipped this
+  // renderer.
+  const compareValue = getIn(evaluationScope, when) ?? '';
   const conditionSatisfied = eq === compareValue;
 
   // note that we return whether the component is hidden, not whether it is shown, so
   // we must invert in the return value
   return conditionSatisfied ? !show : show;
+};
+
+/**
+ * Extract the desired `clearOnHide` behaviour.
+ *
+ * @note Formio's default value is `true`, meaning that values of hidden components
+ * get cleared unless specified otherwise. So even if we get `undefined` or `null`,
+ * the component should be cleared on hide.
+ */
+export const getClearOnHide = (componentDefinition: AnyComponentSchema): boolean => {
+  if ('clearOnHide' in componentDefinition) {
+    return componentDefinition.clearOnHide !== false;
+  }
+  return true;
 };
