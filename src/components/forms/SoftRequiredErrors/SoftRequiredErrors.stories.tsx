58d2124c94c128114b6dcf781a0e2e710426923d
robinvandermolen <robin@maykinmedia.nl>
2025-08-04 15:28:22 +0200

    :white_check_mark: [#108] Added storybook tests for SoftRequiredErrors

--- src/components/forms/SoftRequiredErrors/SoftRequiredErrors.stories.tsx
+++ src/components/forms/SoftRequiredErrors/SoftRequiredErrors.stories.tsx
@@ -1,6 +1,12 @@
-import {Meta, StoryObj} from '@storybook/react-vite';
+import type {
+  SoftRequiredErrorsComponentSchema,
+  TextFieldComponentSchema,
+} from '@open-formulieren/types';
+import {StoryObj} from '@storybook/react-vite';
 import '@utrecht/components/paragraph';
+import {expect, fn, within} from 'storybook/test';
 
+import {renderMultipleComponentsInForm} from '@/registry/storybook-helpers';
 import {withFormik} from '@/sb-decorators';
 
 import SoftRequiredErrors from './SoftRequiredErrors';
@@ -8,23 +14,52 @@ import SoftRequiredErrors from './SoftRequiredErrors';
 export default {
   title: 'Internal API / Forms / SoftRequiredErrors',
   component: SoftRequiredErrors,
+  render: renderMultipleComponentsInForm,
   decorators: [withFormik],
   parameters: {
     formik: {
-      disable: true,
+      initialValues: {
+        textfield: '',
+      },
     },
   },
   args: {
-    html: `
-    <p>Not all required fields are filled out. That can get expensive!</p>
+    onSubmit: fn(),
+    componentDefinitions: [
+      {
+        type: 'textfield',
+        key: 'textfield',
+        label: 'Soft required text',
+        // @ts-ignore
+        openForms: {softRequired: true},
+      } satisfies TextFieldComponentSchema,
+      {
+        id: 'softRequiredErrors',
+        type: 'softRequiredErrors',
+        key: 'softRequiredErrors',
+        label: 'softRequiredErrors',
+        html: `
+        <p>Not all required fields are filled out. That can get expensive!</p>
 
-    {{ missingFields }}
+        {{ missingFields }}
 
-    <p>Are you sure you want to continue?</p>
-      `,
+        <p>Are you sure you want to continue?</p>
+          `,
+      } satisfies SoftRequiredErrorsComponentSchema,
+    ],
   },
-} satisfies Meta<typeof SoftRequiredErrors>;
+};
 
-type Story = StoryObj<typeof SoftRequiredErrors>;
+type Story = StoryObj<typeof renderMultipleComponentsInForm>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+
+    await canvas.findByText('Not all required fields are filled out. That can get expensive!');
+    const list = await canvas.findByRole('list', {name: 'Empty fields'});
+    const listItem = within(list).getByRole('listitem');
+
+    expect(listItem.textContent).toEqual('Soft required text');
+  },
+};
