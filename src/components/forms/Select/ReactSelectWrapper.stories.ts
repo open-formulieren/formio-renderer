ef8e3baecba2df9fb4d0a37371c28b4c633b7ee5
Sergei Maertens <sergei@maykinmedia.nl>
2025-08-27 16:46:45 +0200

    :sparkles: [#105] Implement react-select wrapper
    Added a wrapper around our use cases for react-select, taking into
    account:
    
    * styling with NL DS (build on top of utrecht-select)
    * translate the form state values from Formik into the selected
      option(s) passed to react-select
    
    This is *mostly* a port of the Select in the SDK, polishing some
    typescript bits here and there and iterating on the isMulti variant
    with regards to styling.
    
    A future pass to properly set up the class names/design tokens will
    still be necessary.

---
+++ src/components/forms/Select/ReactSelectWrapper.stories.ts
@@ -0,0 +1,47 @@
+import type {Meta, StoryObj} from '@storybook/react';
+
+import ReactSelectWrapper from './ReactSelectWrapper';
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+export default {
+  title: 'Internal API / Forms / Select / ReactSelectWrapper',
+  component: ReactSelectWrapper,
+  args: {
+    options: [
+      {value: 'option-1', label: 'Option 1'},
+      {value: 'option-2', label: 'Option 2'},
+    ],
+  },
+} satisfies Meta<typeof ReactSelectWrapper<Option>>;
+
+type Story = StoryObj<typeof ReactSelectWrapper<Option>>;
+
+export const Single: Story = {
+  args: {
+    formikValue: 'option-2',
+  },
+};
+
+export const SingleNoValue: Story = {
+  args: {
+    formikValue: null,
+  },
+};
+
+export const MultiValue: Story = {
+  args: {
+    isMulti: true,
+    formikValue: ['option-1', 'option-2'],
+  },
+};
+
+export const ForceMenuOpen: Story = {
+  args: {
+    menuIsOpen: true,
+    formikValue: null,
+  },
+};
