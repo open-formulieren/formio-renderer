663a27d2c791511148f1cacf75e99cf160b2857e
Sonny Bakker <sonny871@hotmail.com>
2025-10-01 17:25:19 +0200

    [#104] initial tests

---
+++ src/registry/postalcode/index.tsx
@@ -0,0 +1,38 @@
+import type {PostCodeInputSchema} from '@open-formulieren/types';
+
+import PostalCodeField from '@/components/forms/PostalCodeField';
+import type {RegistryEntry} from '@/registry/types';
+
+import ValueDisplay from './ValueDisplay';
+import isEmpty from './empty';
+import getInitialValues from './initialValues';
+import getValidationSchema from './validationSchema';
+
+export interface FormioPostCodeProps {
+  componentDefinition: PostCodeInputSchema;
+}
+
+export const FormioPostCode: React.FC<FormioPostCodeProps> = ({
+  componentDefinition: {key, label, description, tooltip, validate},
+}) => {
+  return (
+    <PostalCodeField
+      name={key}
+      label={label}
+      tooltip={tooltip}
+      description={description}
+      isRequired={validate.required}
+      placeholder="AAAA-10"
+    />
+  );
+};
+
+const PostalCodeComponent: RegistryEntry<PostCodeInputSchema> = {
+  formField: FormioPostCode,
+  valueDisplay: ValueDisplay,
+  getInitialValues,
+  getValidationSchema,
+  isEmpty,
+};
+
+export default PostalCodeComponent;
