cb0d470280723d8e3eaed7bcf452d24d42d2d11a
Sergei Maertens <sergei@maykinmedia.nl>
2025-10-07 12:45:55 +0200

    :wrench: Configure ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import {ignoreBuildArtifacts} from '@maykinmedia/eslint-config';
+import recommended from '@maykinmedia/eslint-config/recommended';
+import {defineConfig} from 'eslint/config';
+import globals from 'globals';
+
+const config = defineConfig([
+  ignoreBuildArtifacts(['dist', 'storybook-static']),
+  {
+    files: ['**/*.{js,mjs,ts,tsx}'],
+  },
+  ...recommended,
+  {
+    name: 'project/overrides',
+    rules: {
+      // covered by Typescript
+      'react/prop-types': 'off',
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          fixStyle: 'separate-type-imports',
+          prefer: 'type-imports',
+        },
+      ],
+    },
+  },
+  // Unit tests
+  {
+    files: ['**/*.spec.{ts,tsx}', 'vitest.setup.ts'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.vitest,
+      },
+    },
+  },
+]);
+
+export default config;
