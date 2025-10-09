00e0b23a552469f9149d2da24506ff098ca36eb3
Sonny Bakker <sonny@maykinmedia.nl>
2025-10-09 11:32:21 +0200

    [#104] fix eslint errors/warnings

--- src/registry/postcode/empty.ts
+++ src/registry/postcode/empty.ts
@@ -1,6 +1,6 @@
 import type {PostcodeComponentSchema} from '@open-formulieren/types';
 
-import {IsEmpty} from '@/registry/types';
+import type {IsEmpty} from '@/registry/types';
 
 const isEmpty: IsEmpty<PostcodeComponentSchema, string | string[]> = (_component, value) => {
   // Based on the formio textfield implementation https://github.com/formio/formio.js/blob/29939fc9d66f2b95527c90d3cf7729570c3d3010/src/components/textfield/TextField.js#L300
@@ -8,8 +8,10 @@ const isEmpty: IsEmpty<PostcodeComponentSchema, string | string[]> = (_component
     return true;
   }
 
-  const isFalsy = (valueToCheck: any) =>
-    valueToCheck === undefined || valueToCheck.toString().trim().length === 0;
+  const isFalsy = (valueToCheck: unknown) =>
+    valueToCheck === undefined ||
+    valueToCheck === null ||
+    valueToCheck.toString().trim().length === 0;
   const isEmptyArray = Array.isArray(value) && value.length === 1 ? isFalsy(value[0]) : false;
 
   // in edge cases the existing data & component.multiple may be misaligned
