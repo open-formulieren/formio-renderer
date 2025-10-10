c3c958d8d6ece7bce3de9d06d8c514528668077d
Sergei Maertens <sergei@maykinmedia.nl>
2025-10-10 16:24:18 +0200

    :sparkles: [#111] Implement the empty check for addressNL
    The empty check is used in the soft required errors component, and for
    addressNL components the address data being empty has some flavours:
    
    * either it's undefined/null, meaning no value at all is present
    * or all the (visible) input fields are empty-ish (undefined, empty
      string)
    
    As soon as one field is filled out, it is not considered empty. It may
    be invalid, but it's not empty.

---
+++ src/registry/addressNL/empty.spec.ts
@@ -0,0 +1,63 @@
+import type {AddressData, AddressNLComponentSchema} from '@open-formulieren/types';
+import {expect, test} from 'vitest';
+
+import isEmpty from './empty';
+
+test.each([
+  // Empty states
+  [undefined, true],
+  [null, true],
+  [{}, true],
+  [{postcode: '', houseNumber: ''}, true],
+  // Non-empty state
+  [{postcode: '1234 AB', houseNumber: ''}, false],
+  [{postcode: '', houseNumber: '123'}, false],
+  [{postcode: '1234 AB', houseNumber: '123'}, false],
+  [
+    {
+      postcode: '',
+      houseNumber: '',
+      houseNumberAddition: '27',
+    },
+    false,
+  ],
+  [
+    {
+      postcode: '',
+      houseNumber: '',
+      houseLetter: 'Q',
+    },
+    false,
+  ],
+  [
+    {
+      postcode: '',
+      houseNumber: '',
+      streetName: 'Qwop',
+    },
+    false,
+  ],
+  [
+    {
+      postcode: '',
+      houseNumber: '',
+      city: 'Humperthink',
+    },
+    false,
+  ],
+])(
+  'Radio isEmpty compares against defined string with more then 0 characters state of value',
+  (valueToTest: Partial<AddressData> | null | undefined, expected: boolean) => {
+    const component: AddressNLComponentSchema = {
+      type: 'addressNL',
+      key: 'addressNL',
+      id: 'addressNL',
+      label: 'addressNL',
+      deriveAddress: false,
+      layout: 'singleColumn',
+    };
+
+    const result = isEmpty(component, valueToTest);
+    expect(result).toBe(expected);
+  }
+);
