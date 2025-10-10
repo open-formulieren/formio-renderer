2a32ea06acc90bb6552e44098df5539cce5ca572
Sergei Maertens <sergei@maykinmedia.nl>
2025-10-10 17:03:43 +0200

    :sparkles: [#111] Implement the individual pieces of addressNL
    Each possible field present in the addressNL component is defined
    with only the relevant props.

---
+++ src/registry/addressNL/subFields.tsx
@@ -0,0 +1,111 @@
+import {FormattedMessage, defineMessages} from 'react-intl';
+
+import TextField from '@/components/forms/TextField';
+
+export const FIELD_LABELS = defineMessages({
+  postcode: {
+    description: 'Label for addressNL postcode input',
+    defaultMessage: 'Postcode',
+  },
+  houseNumber: {
+    description: 'Label for addressNL houseNumber input',
+    defaultMessage: 'House number',
+  },
+  houseLetter: {
+    description: 'Label for addressNL houseLetter input',
+    defaultMessage: 'House letter',
+  },
+  houseNumberAddition: {
+    description: 'Label for addressNL houseNumberAddition input',
+    defaultMessage: 'House number addition',
+  },
+  streetName: {
+    description: 'Label for addressNL streetName read only result',
+    defaultMessage: 'Street name',
+  },
+  city: {
+    description: 'Label for addressNL city read only result',
+    defaultMessage: 'City',
+  },
+});
+
+interface PostCodeFieldProps {
+  namePrefix: string;
+  isRequired?: boolean;
+}
+
+export const PostCodeField: React.FC<PostCodeFieldProps> = ({namePrefix, isRequired}) => (
+  <TextField
+    name={`${namePrefix}.postcode`}
+    label={<FormattedMessage {...FIELD_LABELS.postcode} />}
+    placeholder="1234 AB"
+    isRequired={isRequired}
+  />
+);
+
+interface HouseNumberFieldProps {
+  namePrefix: string;
+  isRequired?: boolean;
+}
+
+export const HouseNumberField: React.FC<HouseNumberFieldProps> = ({namePrefix, isRequired}) => (
+  <TextField
+    name={`${namePrefix}.houseNumber`}
+    label={<FormattedMessage {...FIELD_LABELS.houseNumber} />}
+    inputMode="numeric"
+    placeholder="123"
+    isRequired={isRequired}
+  />
+);
+
+interface HouseLetterProps {
+  namePrefix: string;
+}
+
+export const HouseLetter: React.FC<HouseLetterProps> = ({namePrefix}) => (
+  <TextField
+    name={`${namePrefix}.houseLetter`}
+    label={<FormattedMessage {...FIELD_LABELS.houseLetter} />}
+  />
+);
+
+interface HouseNumberAdditionProps {
+  namePrefix: string;
+}
+
+export const HouseNumberAddition: React.FC<HouseNumberAdditionProps> = ({namePrefix}) => (
+  <TextField
+    name={`${namePrefix}.houseNumberAddition`}
+    label={<FormattedMessage {...FIELD_LABELS.houseNumberAddition} />}
+  />
+);
+
+interface StreetNameProps {
+  namePrefix: string;
+  isRequired?: boolean;
+  isDisabled: boolean;
+}
+
+export const StreetName: React.FC<StreetNameProps> = ({namePrefix, isRequired, isDisabled}) => (
+  <TextField
+    name={`${namePrefix}.streetName`}
+    label={<FormattedMessage {...FIELD_LABELS.streetName} />}
+    isRequired={isRequired}
+    isDisabled={isDisabled}
+  />
+);
+
+interface CityProps {
+  namePrefix: string;
+  isRequired?: boolean;
+  isDisabled: boolean;
+}
+
+export const City: React.FC<CityProps> = ({namePrefix, isRequired, isDisabled}) => (
+  <TextField
+    name={`${namePrefix}.city`}
+    label={<FormattedMessage {...FIELD_LABELS.city} />}
+    isRequired={isRequired}
+    isDisabled={isDisabled}
+  />
+);
