9e19081a0a61784c48699127240ffa242fb86e4e
robinvandermolen <robin@maykinmedia.nl>
2025-10-14 13:11:04 +0200

    :white_check_mark: [#169] Added storybook test for modal closing behaviour
    I've tried a bunch of different setups, configurations, things, but testing the html behaviour of the dialog element is a huge pain.
    
    In `.spec` tests I've only managed to make the "click close button" case work, when I override the `HTMLDialogElement.prototype.openModal` and `HTMLDialogElement.prototype.close` functions.
    
    For testing with storybook we need an extra event handler on the modal, to capture `Escape` key clicks.
    
    The "click outside modal dialog" case I cannot get to work in either storybook tests or .spec tests. I believe this is because how the dialog `showModal` works, by displaying the dialog over all other html elements, and placing `inert` attributes on all other elements: https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/showModal.

--- src/components/modal/Modal.stories.tsx
+++ src/components/modal/Modal.stories.tsx
@@ -1,7 +1,28 @@
 import type {Meta, StoryObj} from '@storybook/react-vite';
-import {fn} from 'storybook/test';
+import {useState} from 'react';
+import {expect, fn, userEvent, within} from 'storybook/test';
 
+import type {ModalProps} from './Modal';
 import Modal from './Modal';
+
+const ModalWrapper: React.FC<ModalProps> = ({isOpen, closeModal, ...props}) => {
+  const [isModalOpen, setModalOpen] = useState<boolean>(isOpen ?? true);
+
+  // Mimic the "normal" closing behaviour, by updating the `isOpen` property.
+  const onClose = () => {
+    if (closeModal) {
+      closeModal();
+    }
+    setModalOpen(false);
+  };
+  return (
+    <>
+      <p>Text outside the modal dialog</p>
+      {!isModalOpen && <button onClick={() => setModalOpen(true)}>show modal</button>}
+      <Modal isOpen={isModalOpen} closeModal={onClose} {...props} />
+    </>
+  );
+};
 
 export default {
   title: 'Internal API  / Modal',
@@ -21,5 +42,39 @@ export const Default: Story = {};
 export const WithoutTitle: Story = {
   args: {
     title: undefined,
+  },
+};
+
+export const CloseModal: Story = {
+  render: args => <ModalWrapper {...args} />,
+  play: async ({canvasElement, args, step}) => {
+    const canvas = within(canvasElement);
+
+    // Start with modal shown
+    const dialog = await canvas.findByRole('dialog');
+    expect(dialog).toBeVisible();
+
+    await step('Close with close button', async () => {
+      const closeButton = within(dialog).getByRole('button', {name: 'Close modal'});
+      await userEvent.click(closeButton);
+
+      // The `closeModal` arg should have been called, and the modal should no-longer be
+      // visible.
+      await expect(args.closeModal).toHaveBeenCalled();
+      expect(dialog).not.toBeVisible();
+    });
+
+    // Open the modal again
+    await userEvent.click(canvas.getByRole('button', {name: 'show modal'}));
+
+    await step('Close with escape key', async () => {
+      // Hit the escape button on the keyboard
+      await userEvent.keyboard('[Escape]');
+
+      // The `closeModal` arg should have been called, and the modal should no-longer be
+      // visible.
+      await expect(args.closeModal).toHaveBeenCalled();
+      expect(dialog).not.toBeVisible();
+    });
   },
 };
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -38,6 +38,13 @@ const Modal: React.FC<ModalProps> = ({isOpen = false, title, closeModal, childre
       ref={modalRef}
       aria-labelledby={title ? id : undefined}
       onClose={closeModal}
+      onKeyDown={(event: React.KeyboardEvent<HTMLDialogElement>) => {
+        // The `closedby="any"` should handle the "escape" key click in most cases.
+        // This is still needed for storybook, and perhaps some older browsers.
+        if (event.key === 'Escape') {
+          closeModal();
+        }
+      }}
       // eslint-disable-next-line react/no-unknown-property
       closedby="any"
     >
