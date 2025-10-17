72a183a1f601d73c929d7260f70ddb6da185da71
Viktor van Wijk <viktorvanwijk@hotmail.com>
2025-10-17 16:45:42 +0200

    :recycle: [#83] Combine DateTimeField and DateTimePicker

--- src/components/forms/DateTimeField/DateTimeField.tsx
+++ src/components/forms/DateTimeField/DateTimeField.tsx
@@ -1,12 +1,23 @@
-import {FormField} from '@utrecht/component-library-react';
-import {useFormikContext} from 'formik';
-import {useId} from 'react';
+import {FormField, Paragraph, Textbox} from '@utrecht/component-library-react';
+import {formatISO} from 'date-fns';
+import {useField, useFormikContext} from 'formik';
+import {useEffect, useId, useState} from 'react';
+import {flushSync} from 'react-dom';
+import {useIntl} from 'react-intl';
 
+import {FloatingWidget, useFloatingWidget} from '@/components/forms/FloatingWidget';
 import HelpText from '@/components/forms/HelpText';
+import Label from '@/components/forms/Label';
+import Tooltip from '@/components/forms/Tooltip';
 import ValidationErrors from '@/components/forms/ValidationErrors';
+import Icon from '@/components/icons';
 import {useFieldConfig} from '@/hooks';
 
-import DateTimePicker from './DateTimePicker';
+import './DatePicker.scss';
+import DatePickerCalendar from './DatePickerCalendar';
+import {useDateLocaleMeta} from './hooks';
+import {PART_PLACEHOLDERS} from './messages';
+import {parseDateTime} from './utils';
 
 export interface DateTimeFieldProps {
   /**
@@ -47,12 +58,31 @@ export interface DateTimeFieldProps {
    * Latest date that is selectable in the calendar.
    */
   maxDate?: Date;
-  // TODO-83: is this prop relevant here?
-  /**
-   * An array of ISO-8601 (YYYY-MM-DD) date strings, representing not-available dates.
-   */
-  disabledDates?: string[];
+  'aria-describedby'?: string;
 }
+
+interface DateTimePartValues {
+  date: string;
+  time: string;
+}
+type DateTimePart = keyof DateTimePartValues;
+const dateObjectToParts = (datetime: Date | null): DateTimePartValues => {
+  if (!datetime) return {date: '', time: ''};
+
+  // Do not get the date and time using `toISOString()`, as it always returns UTC time. We create
+  // date objects without timezone information, which sets it to Amsterdam time automatically. So
+  // day shifts will occur when the time is close to midnight.
+  const year = String(datetime.getFullYear());
+  // Note: getMonth() returns the month index, so add 1
+  const month = String(datetime.getMonth() + 1).padStart(2, '0');
+  const day = String(datetime.getDate()).padStart(2, '0');
+  const hour = String(datetime.getHours()).padStart(2, '0');
+  const minute = String(datetime.getMinutes()).padStart(2, '0');
+  return {
+    date: [year, month, day].join('-'),
+    time: [hour, minute].join(':'),
+  };
+};
 
 /**
  * Implements a form field to enter/select a date and time.
@@ -60,8 +90,11 @@ export interface DateTimeFieldProps {
  * Consists of a text input with a floating widget that toggles when focussing the field. This
  * floating widget contains a calendar where a date can be selected, and a time input where a
  * time can be selected. Suitable for nearby dates such as appointments.
+ *
+ * Note: currently we only support the 24-hour format for the time. If we want to support AM/PM
+ * notation, we need to extend our custom parser to convert the textbox value to a (valid)
+ * ISO-8601 string.
  */
-// TODO-83: perhaps just put the DateTimePicker code here?
 const DateTimeField: React.FC<DateTimeFieldProps> = ({
   name,
   label,
@@ -71,29 +104,186 @@ const DateTimeField: React.FC<DateTimeFieldProps> = ({
   tooltip,
   minDate,
   maxDate,
-  disabledDates,
+  'aria-describedby': ariaDescribedBy,
 }) => {
-  const id = useId();
-  const {getFieldMeta} = useFormikContext();
   name = useFieldConfig(name);
-  const {error = '', touched} = getFieldMeta(name);
+  const id = useId();
+  const {formatDate, formatMessage} = useIntl();
+  const {validateField} = useFormikContext();
+  const [{value, onBlur, onChange}, {error = '', touched}, {setTouched, setValue}] =
+    useField<string>(name);
+  const {
+    refs,
+    floatingStyles,
+    context,
+    getFloatingProps,
+    getReferenceProps,
+    isOpen,
+    setIsOpen,
+    arrowRef,
+  } = useFloatingWidget();
+  const dateLocaleMeta = useDateLocaleMeta();
 
   const isInvalid = touched && !!error;
   const errorMessageId = isInvalid ? `${id}-error-message` : undefined;
 
+  // Create placeholder
+  const placeholderMap = {
+    day: formatMessage(PART_PLACEHOLDERS.day),
+    month: formatMessage(PART_PLACEHOLDERS.month),
+    year: formatMessage(PART_PLACEHOLDERS.year),
+  };
+  const datePlaceholder = dateLocaleMeta.partsOrder
+    .map(part => placeholderMap[part])
+    .join(dateLocaleMeta.separator);
+  const placeholder = `${datePlaceholder} ${formatMessage(PART_PLACEHOLDERS.hour)}:${formatMessage(PART_PLACEHOLDERS.minute)}`;
+
+  // Value could be anything, but we only try to parse as an ISO-8601 string, because this is what
+  // we set to the field on blur if the date was correctly parsed using the locale meta.
+  const currentDateTime = parseDateTime(value);
+  const dateTimePartsFromValue = dateObjectToParts(currentDateTime);
+  const [{date, time}, setDateTimeParts] = useState<DateTimePartValues>(dateTimePartsFromValue);
+
+  // Synchronize with external changes if we have a value and our date parts don't
+  // reflect it
+  useEffect(
+    () => {
+      const shouldIgnore = currentDateTime === null && (date === '' || time === '');
+      const newValueParts = dateObjectToParts(currentDateTime);
+
+      const isInconsistent = newValueParts.date !== date || newValueParts.time !== time;
+      if (isInconsistent && !shouldIgnore) {
+        setDateTimeParts(newValueParts);
+      }
+    },
+    // we deliberately exclude date and time from the dependencies
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [currentDateTime]
+  );
+
+  const onPartChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // we must cast here since we can't pass the valid input names as a generic.
+    const name = event.target.name as DateTimePart;
+    const value = event.target.value;
+
+    // update the state of the individual parts
+    const newDateParts = {date, time, [name]: value};
+    setDateTimeParts(newDateParts);
+
+    if (newDateParts.date === '' || newDateParts.time === '') {
+      // If one part is missing, treat as if the entire field was cleared.
+      setValue('');
+    } else {
+      // If we have both parts, we can combine them into an ISO-8601 string. This is possible,
+      // because the date picker and time input return a date and time ISO-8601 string,
+      // respectively.
+      setValue(`${newDateParts.date}T${newDateParts.time}:00`);
+    }
+  };
+
+  // If we have a date object, format it according to the locale (and remove comma) and use as the
+  // textbox value. Otherwise, just use the field value directly.
+  // Note: cannot use `timeStyle: 'short'` here, because it will add AM/PM for English locale.
+  const textboxValue =
+    currentDateTime !== null
+      ? formatDate(currentDateTime, {
+          year: 'numeric',
+          month: 'numeric',
+          day: 'numeric',
+          hour: 'numeric',
+          minute: '2-digit',
+          hour12: false,
+        }).replace(',', '')
+      : value;
+
+  const referenceProps = getReferenceProps();
   return (
-    <FormField type="text" invalid={isInvalid} className="utrecht-form-field--openforms">
-      <DateTimePicker
-        name={name}
-        label={label}
-        tooltip={tooltip}
+    <FormField invalid={isInvalid} className="utrecht-form-field--openforms">
+      <Label
+        id={id}
         isRequired={isRequired}
         isDisabled={isDisabled}
-        aria-describedby={errorMessageId}
-        minDate={minDate}
-        maxDate={maxDate}
-        disabledDates={disabledDates}
-      />
+        tooltip={tooltip ? <Tooltip>{tooltip}</Tooltip> : undefined}
+      >
+        {label}
+      </Label>
+      <Paragraph className="openforms-datepicker-textbox">
+        <Textbox
+          name={name}
+          value={textboxValue}
+          onChange={onChange}
+          onBlur={async event => {
+            const value = event.target.value;
+            // Attempt to create a date object using the locale meta
+            const date = parseDateTime(value, dateLocaleMeta);
+            // If we were able to create a date object, format it to an ISO-8601 string and set it
+            // as the field value. Otherwise, just set the entered value to the field directly.
+            // It's up to the validation libraries to check it.
+            const newValue = date
+              ? formatISO(date, {representation: 'complete'}).slice(0, 19) // remove TZ info
+              : value;
+            await setValue(newValue);
+            onBlur(event);
+            await validateField(name);
+          }}
+          className="utrecht-textbox--openforms"
+          id={id}
+          disabled={isDisabled}
+          invalid={touched && !!error}
+          aria-describedby={ariaDescribedBy}
+          placeholder={placeholder}
+          ref={refs.setReference}
+          autoComplete="off"
+          {...referenceProps}
+        />
+        <Icon
+          icon="calendar"
+          className="openforms-datepicker-textbox__calendar-toggle"
+          aria-label={formatMessage({
+            description: 'Datepicker: accessible calendar toggle label',
+            defaultMessage: 'Toggle calendar',
+          })}
+          aria-hidden="false"
+          aria-controls={referenceProps['aria-controls']}
+          aria-expanded={referenceProps['aria-expanded']}
+          aria-haspopup={referenceProps['aria-haspopup']}
+          onClick={() => !isOpen && setIsOpen(true)}
+        />
+      </Paragraph>
+      <FloatingWidget
+        isOpen={isOpen}
+        context={context}
+        setFloating={refs.setFloating}
+        floatingStyles={floatingStyles}
+        getFloatingProps={getFloatingProps}
+        arrowRef={arrowRef}
+        returnFocus={false}
+      >
+        <DatePickerCalendar
+          onCalendarClick={async selectedDate => {
+            flushSync(() => {
+              // Need to truncate, because the selected date is in datetime format
+              const truncated = selectedDate.substring(0, 10);
+              const e = {
+                target: {name: 'date', value: truncated},
+              } as React.ChangeEvent<HTMLInputElement>;
+              onPartChange(e);
+            });
+            await setTouched(true);
+          }}
+          currentDate={currentDateTime ?? undefined}
+          minDate={minDate}
+          maxDate={maxDate}
+        />
+        <Textbox
+          type="time"
+          step="60" // Ensures no seconds are displayed
+          aria-label="time"
+          name="time"
+          onChange={onPartChange}
+          value={time}
+        />
+      </FloatingWidget>
       <HelpText>{description}</HelpText>
       {touched && errorMessageId && <ValidationErrors error={error} id={errorMessageId} />}
     </FormField>
