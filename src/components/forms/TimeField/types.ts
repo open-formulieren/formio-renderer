cbdc4447a49a8092db7b07abb136ba90b453b8a3
Sonny Bakker <sonny871@hotmail.com>
2025-10-17 13:57:05 +0200

    [#110] use TextField with "time" type

--- src/components/forms/TimeField/types.ts
+++
@@ -1,9 +0,0 @@
-export interface TimePartValues {
-  hour: string;
-  minute: string;
-  second: string;
-}
-export type TimePart = keyof TimePartValues;
-
-export type TimeInputParts = Omit<TimePartValues, 'second'>;
-export type TimeInputPart = keyof TimeInputParts;
