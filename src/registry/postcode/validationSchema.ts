0fb098bef12127a589deef11b971088638399d29
Sergei Maertens <sergei@maykinmedia.nl>
2025-10-21 15:34:07 +0200

    :safety_vest: [#43] Support postcode component multiple in validation schema

--- src/registry/postcode/validationSchema.ts
+++ src/registry/postcode/validationSchema.ts
@@ -17,13 +17,20 @@ const getValidationSchema: GetValidationSchema<PostcodeComponentSchema> = (
   componentDefinition,
   intl
 ) => {
-  const {key, validate} = componentDefinition;
+  const {key, validate, multiple} = componentDefinition;
   const required = validate?.required;
 
   let schema: z.ZodFirstPartySchemaTypes = z
     .string()
     .regex(POSTCODE_REGEX, {message: intl.formatMessage(POSTCODE_INVALID_MESSAGE)});
   if (!required) schema = schema.or(z.literal('')).optional();
+
+  if (multiple) {
+    schema = z.array(schema);
+    if (required) {
+      schema = schema.min(1);
+    }
+  }
 
   return {[key]: schema};
 };
