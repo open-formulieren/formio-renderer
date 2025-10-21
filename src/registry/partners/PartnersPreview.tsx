9bc6762bc89c33fe7f62427bf8c8e8f3014c288d
robinvandermolen <robin@maykinmedia.nl>
2025-10-21 17:28:03 +0200

    :sparkles: [#119] Implement partners component
    Implementing the components and functionality of the partners component. Consisting of: displaying partners data and allowing the manual adding of partner details.

---
+++ src/registry/partners/PartnersPreview.tsx
@@ -0,0 +1,54 @@
+import type {PartnerDetails} from '@open-formulieren/types';
+import {DataList, DataListItem, DataListKey, DataListValue} from '@utrecht/component-library-react';
+import {FormattedMessage} from 'react-intl';
+import type {MessageDescriptor} from 'react-intl';
+
+import './PartnersPreview.scss';
+import {FIELD_LABELS} from './subFields';
+import type {ManuallyAddedPartnerDetails} from './types';
+
+export interface PartnerPreviewProps {
+  partners: ManuallyAddedPartnerDetails[] | PartnerDetails[];
+}
+
+const PartnersPreview: React.FC<PartnerPreviewProps> = ({partners}) => {
+  const partnerFields = Object.entries(FIELD_LABELS).map(
+    ([key, label]: [keyof PartnerDetails, MessageDescriptor]) => ({
+      name: key,
+      label: label,
+    })
+  );
+
+  return (
+    <div className="openforms-partners-preview">
+      {partners.map((partner, index) => (
+        <>
+          {/* Divider between partners */}
+          {index > 0 && <hr />}
+
+          <DataList key={index} appearance="rows">
+            {partnerFields.map(({name, label}) => (
+              <DataListItem key={`${index}-${name}`}>
+                <DataListKey>
+                  <FormattedMessage {...label} />
+                </DataListKey>
+                <DataListValue>
+                  {partner[name] || (
+                    <i>
+                      <FormattedMessage
+                        description="Emtpy field message"
+                        defaultMessage="No information provided"
+                      />
+                    </i>
+                  )}
+                </DataListValue>
+              </DataListItem>
+            ))}
+          </DataList>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default PartnersPreview;
