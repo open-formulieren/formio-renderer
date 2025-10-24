7b798b542f7e7bd59f3b18e4a82ff2a568352353
Sonny Bakker <sonny871@hotmail.com>
2025-10-24 17:04:17 +0200

    [#102] add search component

--- src/registry/map/types.ts
+++ src/registry/map/types.ts
@@ -1,16 +1,10 @@
+import {Provider} from 'leaflet-geosearch/dist/providers/index.js';
+
 export interface NearestLookupBody {
   label: string;
 }
 
-// export interface SearchBody {}
-
 export interface MapParameters {
   mapNearestLookup: (lat: string, lng: string) => Promise<NearestLookupBody> | null;
-  // TODO: should this be done through a callback?
-  // see https://github.com/open-formulieren/open-forms-sdk/blob/a8246baa4b39637c144e328d5f2d6312349da312/src/components/Map/LeafletMapSearchControl.tsx#L45
-  // and https://github.com/open-formulieren/open-forms-sdk/blob/a8246baa4b39637c144e328d5f2d6312349da312/src/components/Map/provider.ts#L24
-  // mapSearch: (
-  //   lat: string,
-  //   lng: string
-  // ) => Promise<SearchBody> | null,
+  searchProvider: Provider;
 }
