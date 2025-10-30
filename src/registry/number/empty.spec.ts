94a084adf561b18b07123f0ed849b702779f6bd7
Viktor van Wijk <viktorvanwijk@hotmail.com>
2025-10-30 14:25:25 +0100

    :bug: Fix check whether value of number component is empty
    Can also be `null`

--- src/registry/number/empty.spec.ts
+++ src/registry/number/empty.spec.ts
@@ -6,13 +6,14 @@ import isEmpty from './empty';
 test.each([
   // Empty states
   [undefined, true],
+  [null, true],
   // Non-empty state
   [0, false],
   [1, false],
   [-1, false],
 ])(
   'Number isEmpty compares against defined number with 1 or more characters',
-  (valueToTest: number | undefined, expected: boolean) => {
+  (valueToTest: number | null | undefined, expected: boolean) => {
     const component: NumberComponentSchema = {
       type: 'number',
       key: 'number',
