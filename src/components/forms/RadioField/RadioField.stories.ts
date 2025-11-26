b05b1c0d435901da96764dd51a4c5ceeaaf7fc8f
Sergei Maertens <sergei@maykinmedia.nl>
2025-11-26 11:45:15 +0100

    :white_check_mark: [#218] Add story/test for currently broken interaction
    We can't easily test that the validation error is not present because
    of the async nature - the assertion runs before the validation kicks
    in and the error is displayed.
    
    However, Chromatic will pick up in the snapshot that the error state
    is active and that's incorrect behaviour.

--- src/components/forms/RadioField/RadioField.stories.ts
+++ src/components/forms/RadioField/RadioField.stories.ts
@@ -148,3 +148,33 @@ export const ValidateOnBlur: Story = {
     expect(radioGroup).toHaveAttribute('aria-invalid', 'true');
   },
 };
+
+// don't display validation errors if there's still focus on a radio input
+export const NoErrorWhileFocus: Story = {
+  args: {
+    name: 'radio',
+    label: 'No error displayed while keyboard navigation happens',
+  },
+  parameters: {
+    formik: {
+      initialValues: {
+        radio: '',
+      },
+      zodSchema: z.object({
+        radio: z.any().refine(() => false, {message: 'Always invalid'}),
+      }),
+    },
+  },
+
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+
+    const firstRadio = await canvas.findByLabelText('Sherlock');
+    firstRadio.focus();
+    await userEvent.keyboard('{ArrowDown}');
+
+    const secondRadio = canvas.getByLabelText('Watson');
+    expect(secondRadio).toBeChecked();
+    expect(secondRadio).toHaveFocus();
+  },
+};
