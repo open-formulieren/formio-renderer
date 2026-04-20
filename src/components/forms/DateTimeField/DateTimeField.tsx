443f09a6baa00decf86537b333038f5711ec68f3
Viktor van Wijk <viktorvanwijk@hotmail.com>
2026-04-20 15:59:36 +0200

    :bug: [open-formulieren/open-forms#6163] Fix date(time) validation not applying consistently
    This is an annoying one. I'm not entirely sure why, but when multiple date fields are present in the same step, only the event listeners of the first date field
    seem to be registered and called on click and focus-in events. This causes inconsistent validations depending on which field loses focus. Validation of the first
    field works properly, but the second one only works when interacting with another field or clicking the submit button.
    
    My initial solution involved making the `onFocusOut` callback a stable reference, which seemed to work (though I didn't manually test all the scenarios which are
    now described by storybook tests as well). However, running validation on (almost) every arbitrary click seems a bit overkill to me. Instead of using the 'click'
    and 'focusin' events, I think we can run validation on a 'focusout' event. There was a comment mentioning that support for 'focusout' is only partial, but when
    looking at the docs [1,2], 'focusin' has the exact same support table. The last browser to support both of them is firefox from 2017. I think it should be OK to
    rely on a feature which has been supported for almost a decade.
    
    Note that I also tried a setup with a blur event handler, but that fires before the `onBlur` callback of the textboxes, which causes validation to run on an
    outdated value.
    
    I did an attempt at some minor cleanup regarding calling the Formik field helper function. The types suggest they return promises, but the examples in the docs
    do not use 'await', and the occurrences in our code seem to have a mixture. Cannot determine from the comments and/or commits whether these are used deliberately.
    The current setup seems to work for manually filling in the fields, as well as the tests.
    
    One tiny thing I've noticed is that validation is now also performed when a user clicks away from the current browser tab. Seems like a rare edge case to me, but
    I'll let the reviewer decide :)
    
    [1]: https://developer.mozilla.org/en-US/docs/Web/API/Element/focusout_event
    [2]: https://developer.mozilla.org/en-US/docs/Web/API/Element/focusin_event

--- src/components/forms/DateTimeField/DateTimeField.tsx
+++ src/components/forms/DateTimeField/DateTimeField.tsx
@@ -4,7 +4,6 @@ import {Textbox} from '@utrecht/textbox-react';
 import {formatISO} from 'date-fns';
 import {useField, useFormikContext} from 'formik';
 import {useEffect, useId, useState} from 'react';
-import {flushSync} from 'react-dom';
 import {useIntl} from 'react-intl';
 
 import {DatePicker, DatePickerRoot, DatePickerTrigger} from '@/components/forms/DatePicker';
@@ -159,11 +158,11 @@ const DateTimeField: React.FC<DateTimeFieldProps> = ({
 
   const onPartChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     // we must cast here since we can't pass the valid input names as a generic.
-    const name = event.target.name as DateTimePart;
+    const partName = event.target.name as DateTimePart;
     const value = event.target.value;
 
     // update the state of the individual parts
-    const newDateParts = {date, time, [name]: value};
+    const newDateParts = {date, time, [partName]: value};
     setDateTimeParts(newDateParts);
 
     if (newDateParts.date === '' || newDateParts.time === '') {
@@ -175,6 +174,8 @@ const DateTimeField: React.FC<DateTimeFieldProps> = ({
       // ISO-8601 string, respectively.
       const date_ = parseDateTime(`${newDateParts.date}T${newDateParts.time}:00`)!;
       setValue(formatISO(date_!));
+      setTouched(true);
+      validateField(name);
     }
   };
 
@@ -211,7 +212,7 @@ const DateTimeField: React.FC<DateTimeFieldProps> = ({
                 name={name}
                 value={textboxValue}
                 onChange={onChange}
-                onBlur={async event => {
+                onBlur={event => {
                   const value = event.target.value;
                   // Attempt to create a date object using the locale meta
                   const date = parseDateTime(value, dateLocaleMeta);
@@ -219,7 +220,7 @@ const DateTimeField: React.FC<DateTimeFieldProps> = ({
                   // as the field value. Otherwise, just set the entered value to the field directly.
                   // It's up to the validation libraries to check it.
                   const newValue = date ? formatISO(date, {representation: 'complete'}) : value;
-                  await setValue(newValue);
+                  setValue(newValue);
                   onBlur(event);
                 }}
                 className="utrecht-textbox--openforms"
@@ -236,16 +237,13 @@ const DateTimeField: React.FC<DateTimeFieldProps> = ({
               />
             </Paragraph>
             <DatePicker
-              onCalendarClick={async selectedDate => {
-                flushSync(() => {
-                  // Need to truncate, because the selected date is in datetime format
-                  const truncated = selectedDate.substring(0, 10);
-                  const e = {
-                    target: {name: 'date', value: truncated},
-                  } as React.ChangeEvent<HTMLInputElement>;
-                  onPartChange(e);
-                });
-                await setTouched(true);
+              onCalendarClick={selectedDate => {
+                // Need to truncate, because the selected date is in datetime format
+                const truncated = selectedDate.substring(0, 10);
+                const e = {
+                  target: {name: 'date', value: truncated},
+                } as React.ChangeEvent<HTMLInputElement>;
+                onPartChange(e);
               }}
               currentDate={currentDateTime ?? undefined}
               minDate={minDate}
@@ -262,11 +260,11 @@ const DateTimeField: React.FC<DateTimeFieldProps> = ({
                 onChange={onPartChange}
                 value={time}
                 className="utrecht-textbox--openforms"
-                onKeyDown={async (event: React.KeyboardEvent) => {
+                onKeyDown={(event: React.KeyboardEvent) => {
                   // treat enter key as a submit
                   if (event.key === 'Enter') {
                     setIsOpen(false);
-                    await validateField(name);
+                    validateField(name);
                   }
                 }}
               />
