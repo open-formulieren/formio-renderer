0415b016955417306d640950192de29aae4b07d9
robinvandermolen <robin@maykinmedia.nl>
2026-09-07 15:15:02 +0200

    :bug: [#386] Fixed tooltip content breaking out the page
    The tooltip components will use a dynamic max-inline-size, based on the available space around the tooltip. The maximum inline size is set at 500px.
    
    The dynamic tooltip inline-size doesn't work when the tooltip content is too long and doesn't wrap. To prevent any issues, we prevent overflowing and wrap the content nicely.
    
    (cherry picked from commit 035c42af2db9ea83a3a6c014b2df61c6ec6189bf)
    (cherry picked from commit 3745c595806325bc7f4a0e67a37a42f2d99b5fbf)
    (cherry picked from commit d314a595b355888b07f90a92d9615b99f3bcccb3)
    
    Backport-of: #387

--- src/components/forms/Tooltip.stories.tsx
+++ src/components/forms/Tooltip.stories.tsx
@@ -35,3 +35,40 @@ export const PlacementRight: Story = {
     ),
   ],
 };
+
+export const LongTooltipContent: Story = {
+  args: {
+    children: `Danish sesame snaps soufflé candy cake. Wafer bonbon pudding liquorice
+      cake cake jujubes. Dessert fruitcake dessert caramels muffin lemon drops croissant
+      macaroon. Candy marzipan muffin cake jujubes tootsie roll lollipop. Cookie biscuit
+      cake donut bonbon. Marshmallow sugar plum apple pie lemon drops lemon drops. Pie
+      donut pie pastry chocolate biscuit wafer icing wafer. Biscuit shortbread tiramisu
+      tiramisu gummies sweet sesame snaps. Fruitcake tart muffin dessert lollipop
+      lollipop. Cupcake pie pie pie donut topping muffin jujubes. Pudding apple pie
+      shortbread danish candy canes gummies. Caramels brownie lemon drops jelly beans
+      powder danish candy canes. Sweet roll gingerbread croissant tiramisu cupcake.`,
+  },
+  // The interaction only triggers the tooltip so we can make chromatic snapshots.
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+    await userEvent.keyboard('[Tab]');
+    expect(
+      await canvas.findByText('Danish sesame snaps soufflé candy cake.', {exact: false})
+    ).toBeVisible();
+  },
+};
+
+export const LongUnbrokenTooltipContent: Story = {
+  args: {
+    children:
+      'DanishsesamesnapssoufflécandycakeWaferbonbonpuddingliquoricecakecakejujubesDessertfruitcakedessertcaramelsmuffinlemondropscroissantmacaroonCandymarzipanmuffincakejujubestootsierolllollipopCookiebiscuitcakedonutbonbonMarshmallowsugarplumapplepielemondropslemondropsPiedonutpiepastrychocolatebiscuitwafericingwaferBiscuitshortbreadtiramisutiramisugummiessweetsesamesnapsFruitcaketartmuffindessertlollipoplollipopCupcakepiepiepiedonuttoppingmuffinjujubesPuddingapplepieshortbreaddanishcandycanesgummiesCaramelsbrownielemondropsjellybeanspowderdanishcandycanesSweetrollgingerbreadcroissanttiramisucupcakecake',
+  },
+  // The interaction only triggers the tooltip so we can make chromatic snapshots.
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+    await userEvent.keyboard('[Tab]');
+    expect(
+      await canvas.findByText('Danishsesamesnapssoufflécandycake', {exact: false})
+    ).toBeVisible();
+  },
+};
