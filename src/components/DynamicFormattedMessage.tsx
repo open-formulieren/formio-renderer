7e022dcb3b9de667d99937d355267b1ea2f24f20
Sergei Maertens <sergei@maykinmedia.nl>
2026-09-10 15:56:57 +0200

    :arrow_up: Upgrade to react-intl 10

--- src/components/DynamicFormattedMessage.tsx
+++ src/components/DynamicFormattedMessage.tsx
@@ -1,6 +1,5 @@
-import {formatMessage as coreFormatMessage} from '@formatjs/intl';
-import type {ResolvedIntlConfig} from '@formatjs/intl/src/types';
-import {DEFAULT_INTL_CONFIG} from '@formatjs/intl/src/utils.js';
+import {DEFAULT_INTL_CONFIG, formatMessage as coreFormatMessage} from '@formatjs/intl';
+import type {ResolvedIntlConfig} from '@formatjs/intl';
 import {Subscript, Superscript} from '@utrecht/component-library-react';
 import DOMPurify from 'dompurify';
 import type {FormatXMLElementFn, PrimitiveType} from 'intl-messageformat';
